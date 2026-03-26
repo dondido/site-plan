3f021971bb4daa339f26e40e77e13a29f36aaac8
Dian Dimitrov <dian_t_d@yahoo.co.uk>
2026-03-26 20:15:16 +0000

    Make logo dynamic size

--- docs/catoctin-walk/js/index.js
+++ docs/catoctin-walk/js/index.js
@@ -183,6 +183,7 @@ const insertView = async (text) => {
 };
 const handlePlan = (raw) => {
     plan = raw;
+    document.body.classList.add(location.pathname.split('/')[1]);
     if (plan.name.includes('.')) {
         const $img = document.createElement('img');
         $img.src = plan.name;
